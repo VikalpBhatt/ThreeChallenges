6e46b32171be5fb1333a36cbda3d04aa4d1e8f4d
VikalpBhatt <50988015+VikalpBhatt@users.noreply.github.com>
2021-01-11 01:03:47 +0000

    Update challenge2.sh

--- 3Challenges/Challenge2/challenge2.sh
+++ 3Challenges/Challenge2/challenge2.sh
@@ -1,2 +1,2 @@
 
-aws ec2 describe-instances --instance-ids myinstance1 --output https://github.com/VikalpBhatt/3Challenges/blob/main/Challenge2/metadata.json
+aws ec2 describe-instances --instance-ids myinstance1 --output json > metadata.json
